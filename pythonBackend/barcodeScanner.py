838863cffcd211257320782d9019cf3f8ae8a032
arryllopez <arryllopez7@gmail.com>
2025-02-06 11:35:24 -0500

    detecting specific ISBN type

--- pythonBackend/barcodeScanner.py
+++ pythonBackend/barcodeScanner.py
@@ -2,22 +2,27 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 def scan_barcodes(image_path):
-    # Open the image
+
     image = Image.open(image_path)
     
-    # Decode the barcodes in the image
+    
     barcodes = decode(image)
     
     if barcodes:
         for barcode in barcodes:
             barcode_data = barcode.data.decode('utf-8')
             barcode_type = barcode.type
-            print(f"Found Barcode: {barcode_data}")
-            print(f"Barcode Type: {barcode_type}")
+            
+            #since ISBN barcodes start with 978 or 979
+            if barcode_type == "EAN13" and (barcode_data.startswith("978") or barcode_data.startswith(979)):
+                print (f"Found ISBN Barcode: {barcode_data}")
+                print ("Barcode Type : ISBN-13") 
+            else : 
+                print(f"Found Barcode: {barcode_data}")
+                print("Barcode Type : {barcode_type}")
     else:
         print("No barcodes found.")
         
-# Example usage
 if __name__ == "__main__":
     image_path = r"C:\Users\arryl\Desktop\thrift-scanner\pythonBackend\barcodeImages\aGameOfThrones.jpg"
     scan_barcodes(image_path)
